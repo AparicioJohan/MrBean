f2b93acadbcee0adc590dd7e5eeb1a0b06a244c8
AparicioJohan <j.aparicio@cgiar.org>
2020-10-07 08:54:40 -0500

    Updates

--- R/app_MrBean.R
+++ R/app_MrBean.R
@@ -15,7 +15,7 @@ app_MrBean <- function()
   suppressWarnings(
     pacman::p_load("shiny", "shinydashboard","plotly", "datasets", "SpATS", "lme4", "tools","readxl","shinyBS","purrr",
                  "GGally","ggpubr","corrplot","missMDA","FactoMineR","factoextra","psych","ggpmisc","lmerTest","car",
-                 "data.table","shinytoastr","broom.mixed","shinyjs","DT"," ggvis","shinyalert","shinycssloaders","shinyWidgets",
+                 "data.table","shinytoastr","broom.mixed","shinyjs","DT","ggvis","shinyalert","shinycssloaders","shinyWidgets",
                  "rintrojs","dplyr","shinydashboardPlus","robustbase")
   )
   shiny::shinyAppDir(system.file("examples/",
--- inst/examples/ui.R
+++ inst/examples/ui.R
@@ -74,7 +74,8 @@ shinyUI(
                                                actionLink(inputId = "toAwesome4", label = "Comparison", icon =icon("glyphicon glyphicon-random",lib = "glyphicon"), style = "color: #d9534f"),br()
                                            )
                                          )
-                                         )
+                                         ),
+                                         HTML( "<script data-name='BMC-Widget' src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js' data-id='mrbean' data-description='Support me on Buy me a coffee!' data-message='Thank you for visiting.' data-color='#28a745' data-position='right' data-x_margin='18' data-y_margin='18'></script>" )
                                  ),
 
                                  #------------------------ Data -----------------------------------------
